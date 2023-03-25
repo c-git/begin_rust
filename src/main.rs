602676b98bc9d6b4974b6cbf4cefb28e3b6e3623
Che <43485962+c-git@users.noreply.github.com>
2023-03-25 10:22:08 -0400

    Move code for each week into separate binary

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,4 @@
-use std::io;
-
-fn can_drive(age:i32)->bool{
-    age >= 16
+fn main() {
+    println!("See binaries in bin folder. See next line for example run command");
+    println!("cargo run --bin c4-question-response");
 }
-
-fn main() -> io::Result<()> {
-    println!("Please enter your name");
-    let mut name = String::new();
-    io::stdin().read_line(&mut name)?;
-    name = name.trim().to_string();
-
-    println!("How old are you?");
-    let mut age = String::new();
-    io::stdin().read_line(&mut age)?;
-    let age:i32 = age.trim().parse().expect("Error converting age to a number");
-    let drive = if can_drive(age){
-        "can drive"
-    } else {
-        "cannot drive yet"
-    };
-
-    println!("Hi {name}, you are {age} years old. You {drive}.");
-    Ok(())
-}
-
