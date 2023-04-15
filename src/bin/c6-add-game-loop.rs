e98aa1043971221cef19db23c555aaf7a24d7082
Che <43485962+c-git@users.noreply.github.com>
2023-04-15 10:50:40 -0400

    Added loop with always asking

--- src/bin/c6-add-game-loop.rs
+++ src/bin/c6-add-game-loop.rs
@@ -1,81 +1,32 @@
-use begin_rust::user_input::get_number_w_prompt;
+use begin_rust::user_input::{get_number_w_prompt, get_string_w_prompt};
 use rand::Rng;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut score = 0;
     let mut total_questions = 0;
-    let first_number = rand::thread_rng().gen_range(1..=50);
-    let second_number = rand::thread_rng().gen_range(1..=50);
-    let correct_answer = first_number + second_number;
 
-    let user_answer = get_number_w_prompt(&format!("{first_number} + {second_number} = "))?;
+    loop {
+        let first_number = rand::thread_rng().gen_range(1..=50);
+        let second_number = rand::thread_rng().gen_range(1..=50);
+        let correct_answer = first_number + second_number;
 
-    total_questions += 1;
-    if user_answer == correct_answer {
-        println!("You are correct!");
-        score += 1;
-    } else {
-        println!("You are wrong. The correct answer was {correct_answer}")
+        let user_answer = get_number_w_prompt(&format!("{first_number} + {second_number} = "))?;
+
+        total_questions += 1;
+        if user_answer == correct_answer {
+            println!("You are correct!");
+            score += 1;
+        } else {
+            println!("You are wrong. The correct answer was {correct_answer}")
+        }
+
+        println!("You're score was {score} out of {total_questions}");
+
+        if "no" == &get_string_w_prompt("Do you want to continue (yes/no)? ")? {
+            break;
+        }
     }
-
-    let first_number = rand::thread_rng().gen_range(1..=50);
-    let second_number = rand::thread_rng().gen_range(1..=50);
-    let correct_answer = first_number + second_number;
-
-    let user_answer = get_number_w_prompt(&format!("{first_number} + {second_number} = "))?;
-
-    total_questions += 1;
-    if user_answer == correct_answer {
-        println!("You are correct!");
-        score += 1;
-    } else {
-        println!("You are wrong. The correct answer was {correct_answer}")
-    }
-
-    let first_number = rand::thread_rng().gen_range(1..=50);
-    let second_number = rand::thread_rng().gen_range(1..=50);
-    let correct_answer = first_number + second_number;
-
-    let user_answer = get_number_w_prompt(&format!("{first_number} + {second_number} = "))?;
-
-    total_questions += 1;
-    if user_answer == correct_answer {
-        println!("You are correct!");
-        score += 1;
-    } else {
-        println!("You are wrong. The correct answer was {correct_answer}")
-    }
-
-    let first_number = rand::thread_rng().gen_range(1..=50);
-    let second_number = rand::thread_rng().gen_range(1..=50);
-    let correct_answer = first_number + second_number;
-
-    let user_answer = get_number_w_prompt(&format!("{first_number} + {second_number} = "))?;
-
-    total_questions += 1;
-    if user_answer == correct_answer {
-        println!("You are correct!");
-        score += 1;
-    } else {
-        println!("You are wrong. The correct answer was {correct_answer}")
-    }
-
-    let first_number = rand::thread_rng().gen_range(1..=50);
-    let second_number = rand::thread_rng().gen_range(1..=50);
-    let correct_answer = first_number + second_number;
-
-    let user_answer = get_number_w_prompt(&format!("{first_number} + {second_number} = "))?;
-
-    total_questions += 1;
-    if user_answer == correct_answer {
-        println!("You are correct!");
-        score += 1;
-    } else {
-        println!("You are wrong. The correct answer was {correct_answer}")
-    }
-
-    println!("You're score was {score} out of {total_questions}");
 
     Ok(())
 }
